cf5c8fe23dde0bc5edebec8174927ad84621392c
P-Andris <pillerandras0714@gmail.com>
2023-02-27 12:41:35 +0100

    1.1 Privát adattagok pótolva

--- Aranyhal.java
+++ Aranyhal.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package main.java_dolgozat_20230227;
 
-/**
- *
- * @author piller.a.gabor
- */
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Aranyhal {
+    private final int KIVANSAGOKSZAMA = 3;
+    private int kor;
+    private Map<String, ArrayList<Integer>> kivansagok;
+    private String kifogta;
+
+    public Aranyhal(String kifogta) {
+        this.kifogta = kifogta;
+    }
+
+    public Aranyhal(int kor, String kifogta) {
+        this.kor = kor;
+        this.kifogta = kifogta;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKifogta(String kifogta) {
+        this.kifogta = kifogta;
+    }
     
+    public int kivansagokSzama(String kifogta) {
+        return 0;
+    }
+    
+    public String[] kikkelTalalkozott() {
+        return new String[1];
+    }
+    
+    public boolean kivansagotTeljesit(String kivansag) {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Aranyhal{" + "KIVANSAGOKSZAMA=" + KIVANSAGOKSZAMA + ", kor=" + kor + ", kivansagok=" + kivansagok + ", kifogta=" + kifogta + '}';
+    }
 }
