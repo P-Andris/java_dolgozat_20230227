a374bed86007d195c7d6701401e1a33638cf8f90
P-Andris <pillerandras0714@gmail.com>
2023-02-27 12:39:24 +0100

    1. Adattagok és metódusok ábra alapján

---
+++ Aranyhal.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java_dolgozat_20230227;
+
+/**
+ *
+ * @author piller.a.gabor
+ */
+public class Aranyhal {
+    
+}
